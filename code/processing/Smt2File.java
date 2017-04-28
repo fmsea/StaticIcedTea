59e17200c98e5a142727dd1a908494803d4a0efe
esherman77 <elenasherman@boisestate.edu>
2017-04-27 22:25:56 -0600

    code for processing sat/unsat counts and updated excel data

--- code/processing/Smt2File.java
+++ code/processing/Smt2File.java
@@ -1,7 +1,134 @@
 package processing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Map.Entry;
 public class Smt2File {
-	private Map<String, Map<String,String>> map;
+	public static void main(String[] args) throws FileNotFoundException {
+//		String[] classes = {"BallonFactory", "Base64", "client",
+//						"GeoData", "GeoEngine", "InfBlocks", "InfCode",
+//						"InfTree", "MapViewer", "QRCodeDataBlockReader",
+//						"StructurePanel", "TileRenderor", "WorldController",
+//						"Class11", "Class13", "OneTcas"
+//				};
+		
+		//String[] classes = {"Base64"};
+		String className = "test.BallonFactory";
+		String methodId = "1";
+		String domain = "dom9";
+		
+		//read the path file and get all the number of path from it
+		String pathFileName = "./ScratchData/conditions/paths/"+className+"_"+methodId+".txt";
+		File pathFile = new File(pathFileName);
+
+		if(pathFile.exists()){
+			//get the scanner
+			Scanner pathScan = new Scanner(pathFile);
+			int pathId = 1;
+			while(pathScan.hasNextLine()){
+				String l = pathScan.nextLine();
+				if(!l.isEmpty()){
+					List<Integer> pathData = new ArrayList<Integer>();
+					//create a list of size 4 for it
+					//index 0 -> sat/sat
+					//index 1 -> unsat/sat
+					//index 2 -> sat/unsat
+					//index 3 -> unsat/unsat
+					for(int i=0; i< 4; i++){// each with 4 counts
+						pathData.add(0);
+					}
+					//smt2 file to count sat/unsat for
+					String smt2File = "./ScratchData/results/satunsat/"+
+					className+"_"+methodId+"_"+
+					String.valueOf(pathId)+"_"+domain+"_satunsat.txt";
+
+		
+//		String[] fileNames = {"res1", "res2", "res3", "res4", "res5"};
+//		//for(String className : classes){
+//			for(int dataFile =0 ; dataFile < fileNames.length; dataFile ++) {
+//				int offset = dataFile *4;
+//				String path = "ExperimentData/z3Results/"+domless + "_" + dommore +"/";
+//				String fileName = fileNames[dataFile];
+				//System.out.println("fileName " + fileName);
+				File file = new File(smt2File);
+				Scanner scanner = new Scanner(new FileReader(file));
+				//List<Integer> count = null;
+				while(scanner.hasNext()){
+					String line = scanner.nextLine();
+
+					//if line start with a number
+//					if(line.matches("^[0-9].*")){
+//						//System.out.println(fileName + "\t " + line);
+//						//then get the method signature
+//						String methodSig = "<"+line.split(":<")[1];
+//						//System.out.println(methodSig);
+//						if(!pathData.containsKey(methodSig)){
+//							
+//							pathData.put(methodSig, count);
+//						} else {
+//							count = pathData.get(methodSig);
+//						}
+//					} else {
+						if(line.equals("sat")){
+							//read the next line
+							String line2 = scanner.nextLine();
+							if(line2.equals("sat")){
+								//add to index 0
+								incrementAt(0, pathData);
+							} else if (line2.equals("unsat")){
+								//add to index 2
+								incrementAt(2,pathData);
+							} else {
+								System.out.println("Something wrong1");
+								System.exit(2);
+							}
+						} else if (line.equals("unsat")){
+							//read the next line
+							String line2 = scanner.nextLine();
+							//System.out.println(line + " " + line2);
+							if(line2.equals("sat")){
+								//add to index 1
+								incrementAt(1, pathData);
+							} else if (line2.equals("unsat")){
+								//add to index 3
+								incrementAt(3,pathData);
+							} else {
+								System.out.println("Something wrong2");
+								System.exit(2);
+							}
+						}
+					}
+				//}
+				scanner.close();
+				//System.out.println("methods " + methodToData.keySet().size());
+				//print the map
+				//System.out.print(pathId);
+					for(Integer val : pathData){
+						System.out.print(val+"\t");
+					}
+					System.out.println();
+				System.out.flush();
+				pathId++;
+			}//end reading datafile
+			} 
+			pathScan.close();
+			} else {
+				System.out.println("Cannot find file " + pathFileName);
+			}
+	//	}//end of class
+		//Total methods
+	}
+
+	private static void incrementAt(int index, List<Integer> list){
+		int temp = list.get(index);
+		temp++;
+		list.set(index, temp);
+	}
 
 }
