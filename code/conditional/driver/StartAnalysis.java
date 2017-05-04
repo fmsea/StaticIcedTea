83b56435e16f45adeada50ebc4f3747fd2df8e70
Elena Sherman <esherman@cse.unl.edu>
2017-05-04 17:55:09 -0600

    fresh results without loops

--- code/conditional/driver/StartAnalysis.java
+++ code/conditional/driver/StartAnalysis.java
@@ -33,7 +33,7 @@ public class StartAnalysis {
 //				"test.InfBlocks", "test.InfCodes", "test.InfTree", "test.MapViewer", "test.QRCodeDataBlockReader", 
 //				"test.StructurePanel", "test.TileRenderor", "test.WorldController", "test.Class11", "test.Class13",
 //		};
-		String[] classNames = {"test.ImageData"};
+		String[] classNames = {"test.TIFFFaxEncoder"};
 		String[] domainNames = {"dom9.txt"};
 		//String[] domainNames = {"dom3.txt", "dom2.txt"};
 		String[] symbolic = {"sN"};
@@ -60,7 +60,7 @@ public class StartAnalysis {
 		//read the file:
 
 		//String symbolicOn = "sY";
-		String methodId = "10";
+		String methodId = "2";
 		StartAnalysis.methodId = methodId;
 		File file = new File("./ScratchData/conditions/paths/"+classNames[0]+"_"+methodId+".txt");
 		if(file.exists()){
@@ -68,7 +68,7 @@ public class StartAnalysis {
 				Scanner scan = new Scanner(file);
 				while(scan.hasNextLine()){
 					String ln = scan.nextLine();
-					if(!ln.isEmpty() && ln.equals("1f")){
+					if(!ln.isEmpty()){
 						conditions.add(ln);
 						StartAnalysisScript.writeToFile=true;
 					}
