ec74bf1b4ce8e4dc0d6cdc9f42c59d806e6daaf5
Kenny Ballou <kballou@devnulllabs.io>
2020-12-17 23:33:00 -0700

    Add shell.nix file
    Specify bare environment for the project.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  jdk = pkgs.openjdk8;
+  maven = (pkgs.maven.override {
+    jdk = jdk;
+  });
+  z3 = (pkgs.z3.override {
+    javaBindings = true;
+    jdk = jdk;
+  });
+in pkgs.mkShell {
+  nativeBuildInputs = [
+    jdk
+    maven
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+  buildInputs = [
+    jdk
+    z3
+    z3.lib
+  ];
+
+  Z3_DIR="${z3.lib}";
+  LD_LIBRARY_PATH="${z3.lib}/lib";
+}
