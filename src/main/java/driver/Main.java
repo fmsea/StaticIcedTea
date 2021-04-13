1253e862ea3279c1ebbf6fa089ddcbbd2af6c4ed
Kenny Ballou <kb@devnulllabs.io>
2021-04-13 13:52:23 -0600

    Add main entry point
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

---
+++ src/main/java/driver/Main.java
@@ -0,0 +1,28 @@
+package driver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import driver.commands.*;
+
+@Command(name = "dfa-smt",
+         mixinStandardHelpOptions = true,
+         version = "1.0",
+         description = "Run various dfa-smt utilities",
+         subcommands = {
+         })
+public class Main implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Integer exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+}
