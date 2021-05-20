21785784d27e849635e43e9b70c149602ce49593
Kenny Ballou <kb@devnulllabs.io>
2021-05-20 14:52:29 -0600

    Interval32BoxProvider: add edges cases
    Add several edge cases for Interval32Box property tests.  TOP and BOT
    values are handled outside of the property tests, so we do not introduce
    those here.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/test/java/abstractinterp/scalar/state/providers/Interval32BoxProvider.java
+++ src/test/java/abstractinterp/scalar/state/providers/Interval32BoxProvider.java
@@ -5,11 +5,11 @@ import abstractinterp.scalar.state.Interval32Box;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
 import net.jqwik.api.Combinators;
+import net.jqwik.api.Tuple;
 import net.jqwik.api.providers.ArbitraryProvider;
 import net.jqwik.api.providers.TypeUsage;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Random;
 import java.util.Set;
 
@@ -29,6 +29,18 @@ public class Interval32BoxProvider implements ArbitraryProvider {
         int partition = r.nextInt(max);
         Arbitrary<Integer> lower = Arbitraries.integers().between(min, partition);
         Arbitrary<Integer> upper = Arbitraries.integers().between(partition, max);
-        return Collections.singleton(Combinators.combine(lower, upper).as(Interval32Box::new));
+        Arbitrary<Interval32Box> randomIntervals = Combinators.combine(lower, upper)
+            .as(Interval32Box::new);
+        Arbitrary<Interval32Box> singletons =
+            Arbitraries.frequencyOf(Tuple.of(1, Arbitraries.just(new Interval32Box(0))),
+                                    Tuple.of(1, Arbitraries.just(new Interval32Box(partition))),
+                                    Tuple.of(1, lower.flatMap(x -> Arbitraries.just(new Interval32Box(x)))),
+                                    Tuple.of(1, upper.flatMap(x -> Arbitraries.just(new Interval32Box(x)))));
+        Arbitrary<Interval32Box> rangeOne =
+            Arbitraries.frequencyOf(Tuple.of(1, Arbitraries.just(new Interval32Box(-1, 0))),
+                                    Tuple.of(1, Arbitraries.just(new Interval32Box(0, 1))));
+        return Collections.singleton(Arbitraries.frequencyOf(Tuple.of(5, randomIntervals),
+                                                             Tuple.of(1, singletons),
+                                                             Tuple.of(1, rangeOne)));
     }
 }
