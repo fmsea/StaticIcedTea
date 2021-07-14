5862ed59c5a0c53e9150b33a5fd4c51b1ea3bd28
Kenny Ballou <kb@devnulllabs.io>
2021-07-14 15:13:24 -0600

    ForwardBranchedFlowAnalysis: :rose: reformat and remove whitespace
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/main/java/abstractinterp/scalar/ForwardBranchedFlowAnalysis.java
+++ src/main/java/abstractinterp/scalar/ForwardBranchedFlowAnalysis.java
@@ -10,75 +10,77 @@ import soot.toolkits.graph.DirectedGraph;
 import soot.toolkits.graph.UnitGraph;
 import soot.toolkits.graph.PseudoTopologicalOrderer;
 
-public abstract class ForwardBranchedFlowAnalysis<N extends Unit, A> extends ForwardBranchedFlowBasic<N, A> {
+public abstract class ForwardBranchedFlowAnalysis<N extends Unit, A>
+        extends ForwardBranchedFlowBasic<N, A> {
 
-	public ForwardBranchedFlowAnalysis(DirectedGraph<N> graph) {
-		super(graph);
-		//populate order
-		unitToAfterFallFlow = new HashMap<N, List<A>>(graph.size() * 2 + 1, 0.7f);
-		unitToAfterBranchFlow = new HashMap<N, List<A>>(graph.size() * 2 + 1, 0.7f);
-		unitToBeforeFlow = new HashMap<N, A>(graph.size() * 2 + 1, 0.7f);
-		order = new PseudoTopologicalOrderer<N>().newList(graph, false);
+    public ForwardBranchedFlowAnalysis(DirectedGraph<N> graph) {
+        super(graph);
+        // populate order
+        unitToAfterFallFlow = new HashMap<>(graph.size() * 2 + 1, 0.7f);
+        unitToAfterBranchFlow = new HashMap<>(graph.size() * 2 + 1, 0.7f);
+        unitToBeforeFlow = new HashMap<>(graph.size() * 2 + 1, 0.7f);
+        order = new PseudoTopologicalOrderer<N>().newList(graph, false);
 
-	}
+    }
 
-	@Override
-	protected void doAnalysis(){
-		//initialize the flows as done in the original implementation
-		for(N node : order){
-			unitToBeforeFlow.put(node, newInitialFlow());
-			List<A> f = new ArrayList<A>();
-			unitToAfterFallFlow.put(node, f);
-			if(node.fallsThrough()){
-				f.add(newInitialFlow());
-			} 
-			f = new ArrayList<A>();
-			unitToAfterBranchFlow.put(node, f);
-			if(node.branches()){
-				for(int i=0; i < node.getUnitBoxes().size(); i++){
-					A v = newInitialFlow();
-					f.add(v);
-				}
-			}
-			//entry points
-			for(N head : graph.getHeads()){
-				unitToBeforeFlow.put(head, entryInitialFlow());
-			}
+    @Override
+    protected void doAnalysis() {
+        // initialize the flows as done in the original implementation
+        for (N node : order) {
+            unitToBeforeFlow.put(node, newInitialFlow());
+            List<A> f = new ArrayList<>();
+            unitToAfterFallFlow.put(node, f);
+            if (node.fallsThrough()) {
+                f.add(newInitialFlow());
+            }
+            f = new ArrayList<A>();
+            unitToAfterBranchFlow.put(node, f);
+            if (node.branches()) {
+                for (int i = 0; i < node.getUnitBoxes().size(); i++) {
+                    A v = newInitialFlow();
+                    f.add(v);
+                }
+            }
+            // entry points
+            for (N head : graph.getHeads()) {
+                unitToBeforeFlow.put(head, entryInitialFlow());
+            }
 
-			//traps are treated as entry points
-			if(treatTrapHandlersAsEntries()){
-				for(Trap trap : ((UnitGraph) graph).getBody().getTraps()){
-					Unit hanlder = trap.getHandlerUnit();
-					unitToBeforeFlow.put((N) hanlder, entryInitialFlow());
-				}
-			}
+            // traps are treated as entry points
+            if (treatTrapHandlersAsEntries()) {
+                for (Trap trap : ((UnitGraph) graph).getBody().getTraps()) {
+                    Unit hanlder = trap.getHandlerUnit();
+                    unitToBeforeFlow.put((N) hanlder, entryInitialFlow());
+                }
+            }
 
-		}
-		super.doAnalysis();
-	}
+        }
+        super.doAnalysis();
+    }
 
-	protected boolean isFowrad(){
-		return true;
-	}
+    protected boolean isFowrad() {
+        return true;
+    }
 
-	/**
-	 * Returns the initial flow value for entry/exit graph nodes.
-	 * 
-	 * This is equal to {@link #newInitialFlow()}
-	 */
-	protected A entryInitialFlow() {
-		return newInitialFlow();
-	}
+    /**
+     * Returns the initial flow value for entry/exit graph nodes.
+     *
+     * This is equal to {@link #newInitialFlow()}
+     */
+    protected A entryInitialFlow() {
+        return newInitialFlow();
+    }
 
-	/**
-	 * Determines whether <code>entryInitialFlow()</code>
-	 * is applied to trap handlers.
-	 */
-	protected boolean treatTrapHandlersAsEntries() { return false; }
-	
-	@Override
-	protected void mergeFlows(N node, A beforeFlow, List<A> preds){
-		basicMergeFlows(node, beforeFlow, preds);
-	}
+    /**
+     * Determines whether <code>entryInitialFlow()</code> is applied to trap handlers.
+     */
+    protected boolean treatTrapHandlersAsEntries() {
+        return false;
+    }
+
+    @Override
+    protected void mergeFlows(N node, A beforeFlow, List<A> preds) {
+        basicMergeFlows(node, beforeFlow, preds);
+    }
 
 }
