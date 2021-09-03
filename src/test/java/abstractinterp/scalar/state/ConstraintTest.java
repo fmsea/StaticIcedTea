81d69252d4fd565c9fafe506ad51cd70b644aea4
Kenny Ballou <kb@devnulllabs.io>
2021-09-03 11:47:50 -0600

    tests: Constraints: refactor: use static imports
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/test/java/abstractinterp/scalar/state/ConstraintTest.java
+++ src/test/java/abstractinterp/scalar/state/ConstraintTest.java
@@ -1,22 +1,25 @@
 package abstractinterp.scalar.state;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class ConstraintTest {
 
     @Test
     void testIsTop() {
-        Assertions.assertFalse(Constraint.BOT().isTop());
-        Assertions.assertFalse(new Constraint(0, PredicateType.Lt).isTop());
-        Assertions.assertTrue(Constraint.TOP().isTop());
+        assertAll(() -> assertFalse(Constraint.BOT().isTop()),
+                  () -> assertFalse(new Constraint(0, PredicateType.Lt).isTop()),
+                  () -> assertTrue(Constraint.TOP().isTop()));
     }
 
     @Test
     void testConstraintTopNegate() {
-        Assertions.assertEquals(new Constraint(Integer.MIN_VALUE + 1,
-                                               PredicateType.Ge),
-                                Constraint.TOP().negate());
+        assertEquals(new Constraint(Integer.MIN_VALUE + 1,
+                                    PredicateType.Ge),
+                     Constraint.TOP().negate());
     }
 
     @Test
@@ -24,14 +27,14 @@ public class ConstraintTest {
         Constraint x = new Constraint(Integer.MAX_VALUE - 2,
                                       PredicateType.Eq);
         Constraint y = new Constraint(3, PredicateType.Eq);
-        Assertions.assertEquals(Integer.MAX_VALUE, x.add(y).bound());
+        assertEquals(Integer.MAX_VALUE, x.add(y).bound());
     }
 
     @Test
     void testConstraintSubtractionUnderflow() {
         Constraint top = Constraint.TOP().negate();
-        Assertions.assertEquals(Integer.MIN_VALUE + 1,
-                                top.subtract(new Constraint(1, PredicateType.Eq)).bound());
+        assertEquals(Integer.MIN_VALUE + 1,
+                     top.subtract(new Constraint(1, PredicateType.Eq)).bound());
     }
 
     @Test
@@ -39,14 +42,14 @@ public class ConstraintTest {
         Constraint x = new Constraint(Integer.MAX_VALUE - 20,
                                       PredicateType.Eq);
         Constraint y = new Constraint(2, PredicateType.Eq);
-        Assertions.assertEquals(Constraint.TOP(), x.multiply(y));
+        assertEquals(Constraint.TOP(), x.multiply(y));
     }
 
     @Test
     void testConstraintDivisionOverflow() {
         Constraint x = new Constraint(Integer.MIN_VALUE, PredicateType.Eq);
         Constraint y = new Constraint(-1, PredicateType.Eq);
-        Assertions.assertEquals(Constraint.TOP(), x.divide(y));
+        assertEquals(Constraint.TOP(), x.divide(y));
     }
 
     @Test
@@ -55,39 +58,41 @@ public class ConstraintTest {
 
     @Test
     void testBottomConstraintsAreBottom() {
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.TOP().add(Constraint.BOT()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.BOT().add(Constraint.TOP()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.TOP().subtract(Constraint.BOT()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.BOT().subtract(Constraint.TOP()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.TOP().multiply(Constraint.BOT()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.BOT().multiply(Constraint.TOP()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.TOP().divide(Constraint.BOT()));
-        Assertions.assertEquals(Constraint.BOT(),
-                                Constraint.BOT().divide(Constraint.TOP()));
+        assertAll(() -> assertEquals(Constraint.BOT(),
+                                     Constraint.TOP().add(Constraint.BOT())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.BOT().add(Constraint.TOP())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.TOP().subtract(Constraint.BOT())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.BOT().subtract(Constraint.TOP())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.TOP().multiply(Constraint.BOT())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.BOT().multiply(Constraint.TOP())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.TOP().divide(Constraint.BOT())),
+                  () -> assertEquals(Constraint.BOT(),
+                                     Constraint.BOT().divide(Constraint.TOP())));
     }
 
     @Test
     void testConstraintMax() {
-        Assertions.assertEquals(new Constraint(0),
-                                Constraint.max(new Constraint(0), new Constraint(-1)));
-        Assertions.assertEquals(new Constraint(0),
-                                Constraint.max(new Constraint(-1), new Constraint(0)));
-        Assertions.assertEquals(new Constraint(0, PredicateType.Le),
-                                Constraint.max(new Constraint(+0, PredicateType.Le),
-                                               new Constraint(-1, PredicateType.Le)));
-        Assertions.assertEquals(new Constraint(2, PredicateType.Le),
-                                Constraint.max(new Constraint(-2, PredicateType.Le),
-                                               new Constraint(+2, PredicateType.Le)));
-        Assertions.assertEquals(new Constraint(0, PredicateType.Le),
-                                Constraint.max(new Constraint(0, PredicateType.Le), new Constraint(0)));
-        Assertions.assertEquals(new Constraint(0, PredicateType.Le),
-                                Constraint.max(new Constraint(0), new Constraint(0, PredicateType.Le)));
+        assertAll(() -> assertEquals(new Constraint(0),
+                                      Constraint.max(new Constraint(0), new Constraint(-1))),
+                  () -> assertEquals(new Constraint(0),
+                                     Constraint.max(new Constraint(-1), new Constraint(0))),
+                  () -> assertEquals(new Constraint(0, PredicateType.Le),
+                                     Constraint.max(new Constraint(+0, PredicateType.Le),
+                                                    new Constraint(-1, PredicateType.Le))),
+                  () -> assertEquals(new Constraint(2, PredicateType.Le),
+                                     Constraint.max(new Constraint(-2, PredicateType.Le),
+                                                    new Constraint(+2, PredicateType.Le))),
+                  () -> assertEquals(new Constraint(0, PredicateType.Le),
+                                     Constraint.max(new Constraint(0, PredicateType.Le),
+                                                    new Constraint(0))),
+                  () -> assertEquals(new Constraint(0, PredicateType.Le),
+                                     Constraint.max(new Constraint(0),
+                                              new Constraint(0, PredicateType.Le))));
     }
 }
