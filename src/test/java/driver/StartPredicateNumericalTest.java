fc372cb1c1276588ad112011afe85cfdd7aa5cbd
Kenny Ballou <kb@devnulllabs.io>
2021-10-15 17:54:38 -0600

    tests: add start predicate numerical test
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

---
+++ src/test/java/driver/StartPredicateNumericalTest.java
@@ -0,0 +1,45 @@
+package driver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import util.Compiler;
+
+import driver.providers.PredicateMiniJavaExamplesProvider;
+
+public class StartPredicateNumericalTest {
+
+    @ParameterizedTest
+    @ArgumentsSource(PredicateMiniJavaExamplesProvider.class)
+    void testPredicateNumericalAnalysis(String name, String source, String expected) throws Exception {
+        Path clazz = Compiler.compileSource(name, source);
+
+        Process analysis = Runtime.getRuntime().exec(new String[] {
+                "java",
+                "-classpath",
+                System.getProperty("java.class.path"),
+                "driver.StartPredicateNumerical",
+                name,
+                clazz.getParent().toString(),
+                "1",
+                "analysis/ExperimentData/domains/dom3.txt",
+                "sY",
+            });
+        analysis.waitFor(60l, TimeUnit.SECONDS);
+        String out = new BufferedReader(new InputStreamReader(analysis.getInputStream(),
+                                                              StandardCharsets.UTF_8))
+            .lines()
+            .collect(Collectors.joining("\n"));
+
+        assertEquals(expected, out);
+    }
+}
---
+++ src/test/java/driver/providers/PredicateMiniJavaExamplesProvider.java
@@ -0,0 +1,19 @@
+package driver.providers;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+
+public class PredicateMiniJavaExamplesProvider extends MiniJavaExamplesProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext)
+        throws Exception {
+
+        return Stream.of(example("miniExample1", "predicate"),
+                         example("miniExample2", "predicate"),
+                         example("miniExample3", "predicate"),
+                         example("miniExample4", "predicate"),
+                         example("miniExample5", "predicate"));
+    }
+}
