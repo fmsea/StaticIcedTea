9ecb1afc5242fdf2e3433a638780d01eea60699f
Kenny Ballou <kb@devnulllabs.io>
2021-12-06 17:07:58 -0700

    tests: add IncDBS<=>PADO01DBS comparative tests
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

---
+++ src/test/java/abstractinterp/scalar/IncPADO01ComparativeTest.java
@@ -0,0 +1,241 @@
+package abstractinterp.scalar;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.FileWriter;
+import java.io.StringReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import soot.Scene;
+import soot.Body;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import abstractinterp.scalar.state.PADO01DifferenceBoundedState;
+import abstractinterp.scalar.state.IncDifferenceBoundedState;
+import abstractinterp.scalar.state.factory.PADO01DifferenceBoundedStateFactory;
+import abstractinterp.scalar.state.factory.IncDifferenceBoundedStateFactory;
+import abstractinterp.scalar.state.providers.JimpleProvider;
+import processing.Smt2Format;
+import solver.SolverWrapper;
+import solver.SolverWrapperZ3;
+
+public class IncPADO01ComparativeTest extends AbstractNumericalTest {
+
+    private SolverWrapper solver;
+    private Path z3TestFile;
+
+    @BeforeEach
+    void setup() {
+        try {
+            this.solver = new SolverWrapperZ3();
+            this.z3TestFile = Files.createTempFile("dfa-smt",
+                                                   String.valueOf(System.nanoTime()));
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    @AfterEach
+    void teardown() {
+        try {
+            Files.deleteIfExists(this.z3TestFile);
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    @BeforeAll
+    static void sootSuiteInitialize() {
+        Scene.v().loadClassAndSupport("java.lang.Object");
+        Scene.v().loadClassAndSupport("java.lang.System");
+        Scene.v().loadNecessaryClasses();
+    }
+
+    @Test
+    void testConstantValue() {
+        Body body = JimpleProvider.constantJimpleMethod("z3_constant_test");
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.constantValuePropagation.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testConstantArithmatic() {
+        Body body = JimpleProvider.binaryArithmaticMethod("z3ConstantMath");
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.constantMathPropagation.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testBranchingStatement() {
+        Body body = JimpleProvider.simpleIfStatement("z3_simpleIf");
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.branching.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testLoopingStatement() {
+        Body body = JimpleProvider.simpleLoopStatement("z3_simple_loop");
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.looping.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testExample5() {
+        Body body = JimpleProvider.example5();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.example5.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testNonsenseExample() {
+        Body body = JimpleProvider.nonsense();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.nonsenseExample.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testNeqLoop() {
+        Body body = JimpleProvider.neqLoop();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.neqLoop.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testGetArrowSubset() {
+        Body body = JimpleProvider.ballonGetArrow();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.getArrowSubset.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testIntervalComparison() {
+        Body body = JimpleProvider.intervalComparison();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.intervalComparison.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    @Test
+    void testFibonacci() {
+        Body body = JimpleProvider.fibonacci();
+        IntegerAnalysis<IncDifferenceBoundedState> analysis1 =
+            new IntegerAnalysis<>(this.solver, body, 2, new IncDifferenceBoundedStateFactory());
+        IntegerAnalysis<PADO01DifferenceBoundedState> analysis2 =
+            new IntegerAnalysis<>(this.solver, body, 2, new PADO01DifferenceBoundedStateFactory());
+        analysis1.runAnalysis();
+        analysis2.runAnalysis();
+        String expected = readResourcesFile("inc.pado01.fibonacci.smt.out");
+        Assertions.assertTrue(runComparison(analysis1.generateSMTReport(),
+                                            analysis2.generateSMTReport(),
+                                            expected));
+    }
+
+    private boolean runComparison(String left, String right, String oracle) {
+        Reader leftReader = new StringReader(left);
+        Reader rightReader = new StringReader(right);
+        try {
+            Writer writer = new FileWriter(this.z3TestFile.toFile());
+            Smt2Format.SMT2Format(leftReader, rightReader, writer);
+            Process z3 = Runtime.getRuntime().exec(new String[] {"z3",
+                                                                 "-smt2",
+                                                                 this.z3TestFile.toString()});
+            z3.waitFor(60l, TimeUnit.SECONDS);
+            String output = new BufferedReader(new InputStreamReader(z3.getInputStream(),
+                                                                     StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+            Assertions.assertEquals(oracle, output);
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+            return false;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace(System.err);
+            return false;
+        }
+    }
+}
