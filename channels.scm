5d2a359651542d2910dff6ed0238d83a8d8180b5
Kenny Ballou <kb@devnulllabs.io>
2022-06-30 17:03:00 -0600

    guix: add personal channel
    This channel includes updated z3 and "java-z3".
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- channels.scm
+++ channels.scm
@@ -1,4 +1,8 @@
 (list (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit "b80ca672de936a76368de6e6ea0b28505e74d420")))
+       (commit "9829f53a3f88884e245e962073528be46918ad2e"))
+      (channel
+       (name 'kb-guix)
+       (url "https://git.sr.ht/~kennyballou/guix.git")
+       (commit "2015735a5edf455d1cfcd54719cd96696507481f")))
