9402c8d16c1c8a5b9b1ddf96d3a7ae7a969737a6
Kenny Ballou <kb@devnulllabs.io>
2022-09-14 01:11:25 -0600

    guix: update channel
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- channels.scm
+++ channels.scm
@@ -1,8 +1,4 @@
 (list (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit "9829f53a3f88884e245e962073528be46918ad2e"))
-      (channel
-       (name 'kb-guix)
-       (url "https://git.sr.ht/~kennyballou/guix.git")
-       (commit "2015735a5edf455d1cfcd54719cd96696507481f")))
+       (commit "6199ee19ff84f904972fcc703442dff24018ef4d")))
