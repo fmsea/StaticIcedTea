a1865488b48f3c686d080cc84c249dcd3a7a650f
Kenny Ballou <kb@devnulllabs.io>
2023-03-20 13:05:44 -0600

    Locals: add convenience method for set of Locals
    Often in tests, it is necessary to return a set of Local's.
    Syntactically, this can be slightly tedious, add a simple convenience
    method to simplify construction.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/main/java/processing/Locals.java
+++ src/main/java/processing/Locals.java
@@ -1,9 +1,12 @@
 package processing;
 
+import java.util.Set;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import soot.Local;
 import soot.IntType;
 import soot.jimple.Jimple;
@@ -17,6 +20,12 @@ public class Locals {
     public static Local get(String id) {
         locals.putIfAbsent(id, Jimple.v().newLocal(id, IntType.v()));
         return locals.get(id);
+    }
+
+    public static Set<Local> get(String... ids) {
+        return Stream.of(ids)
+            .map(id -> Locals.get(id))
+            .collect(Collectors.toSet());
     }
 
     public static Collection<Local> values() {
