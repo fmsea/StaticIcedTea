9e9f61bedf1c464dd588d4d61dd7b89f78c68068
Kenny Ballou <kb@devnulllabs.io>
2023-03-22 15:39:32 -0600

    SmtExpression: false always returns false
    Override toSmt2 at all levels to ensure that "false" is printed.  This
    is semantically better than something like `(= 0 1)`.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/main/java/processing/smt/FalseSmtExpression.java
+++ src/main/java/processing/smt/FalseSmtExpression.java
@@ -18,21 +18,46 @@ public class FalseSmtExpression extends SmtExpression {
         return Jimple.v().newEqExpr(IntConstant.v(0), IntConstant.v(1));
     }
 
+    @Override
     public Optional<Value> getValue(Local id) {
         return Optional.empty();
     }
 
+    @Override
     public Optional<Value> getValue(Set<Local> variables) {
         return Optional.empty();
     }
 
+    @Override
     public Optional<Value> getReachableValue(Set<Local> sources) {
         return Optional.empty();
     }
 
+    @Override
     public String toSmt2() {
         return "false";
     }
+
+    @Override
+    public Optional<String> toSmt2(Local id) {
+        return Optional.of("false");
+    }
+
+    @Override
+    public Optional<String> toSmt2(Set<Local> variables) {
+        return Optional.of("false");
+    }
+
+    @Override
+    public Optional<String> toReachableSmt2(Local id) {
+        return Optional.of("false");
+    }
+
+    @Override
+    public Optional<String> toReachableSmt2(Set<Local> sources) {
+        return Optional.of("false");
+    }
+
 
     public boolean containsAll(Set<Local> variables) {
         return variables.size() == 0;
