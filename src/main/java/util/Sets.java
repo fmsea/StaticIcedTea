ec298fbe9543c5eff0e0de0bf53aa352e4b94962
Kenny Ballou <kb@devnulllabs.io>
2023-04-30 15:03:23 -0600

    Add simple Sets utility class
    Often, we need some version of one of these functions.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

---
+++ src/main/java/util/Sets.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Sets {
+
+    public static <T> Set<T> union(Set<T> left, Set<T> right) {
+        return Stream.concat(left.stream(),
+                             right.stream())
+            .collect(Collectors.toSet());
+    }
+
+    public static <T> Set<T> difference(Set<T> left, Set<T> right) {
+        return left.stream().filter(e -> !right.contains(e)).collect(Collectors.toSet());
+    }
+
+    public static <T> boolean equal(Set<T> left, Set<T> right) {
+        return left.containsAll(right) && right.containsAll(left);
+    }
+
+    public static <T> boolean subset(Set<T> left, Set<T> right) {
+        return right.containsAll(left);
+    }
+}
