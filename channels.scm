39f1a9621204194f1975ef16e73999e414f1fc3f
Kenny Ballou <kb@devnulllabs.io>
2023-05-22 15:04:41 -0600

    guix: add channel introduction
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- channels.scm
+++ channels.scm
@@ -1,4 +1,9 @@
 (list (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit "dff1689bb37e5303868584d3f1d7a33cbcb7f51e")))
+       (commit "dff1689bb37e5303868584d3f1d7a33cbcb7f51e")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
