85474aed0059370fd271739c35576819adf3e610
Kenny Ballou <kb@devnulllabs.io>
2023-09-12 10:36:33 -0600

    refactor constraint property tests
    Move constraint property tests to correct class file.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/test/java/abstractinterp/scalar/state/ConstraintProperties.java
+++ src/test/java/abstractinterp/scalar/state/ConstraintProperties.java
@@ -6,6 +6,16 @@ import net.jqwik.api.Property;
 public class ConstraintProperties {
 
     @Property
+    boolean testCompareToTop(@ForAll Constraint c) {
+        return Constraint.compare(c, Constraint.TOP()) == -1;
+    }
+
+    @Property
+    boolean testCompareToBot(@ForAll Constraint c) {
+        return Constraint.compare(c, Constraint.BOT()) == 1;
+    }
+
+    @Property
     boolean testCopy(@ForAll Constraint c) {
         Constraint d = c.copy();
         return d.equals(c) && d.hashCode() == c.hashCode();
--- src/test/java/abstractinterp/scalar/state/DifferenceBoundedMatrixProperties.java
+++ src/test/java/abstractinterp/scalar/state/DifferenceBoundedMatrixProperties.java
@@ -3,10 +3,5 @@ package abstractinterp.scalar.state;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 
-public class DifferenceBoundedMatrixProperties {
-
-    @Property
-    boolean testCompareToTop(@ForAll Constraint c) {
-        return Constraint.compare(c, Constraint.TOP()) == -1;
-    }
+public class DifferenceBoundedMatrixProperties { 
 }
