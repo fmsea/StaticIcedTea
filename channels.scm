655502d05924e0dd7c2c1a434517e632adde47d3
Kenny Ballou <kb@devnulllabs.io>
2023-09-12 10:46:07 -0600

    channels: temporary use mirror for guix channel
    Updates on the main channel's repository do not seem to be working
    correctly.  However, pointing to a mirror of the repository appears to
    work.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- channels.scm
+++ channels.scm
@@ -1,6 +1,6 @@
 (list (channel
        (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
+       (url "https://git.sr.ht/~kennyballou/guix.git")
        (commit "dff1689bb37e5303868584d3f1d7a33cbcb7f51e")
        (introduction
         (make-channel-introduction
