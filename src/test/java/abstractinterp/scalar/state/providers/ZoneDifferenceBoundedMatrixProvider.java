4c6b53a345ccb0b8baf246055bccd27731e7a285
Kenny Ballou <kb@devnulllabs.io>
2024-05-27 06:23:32 -0600

    ZoneDBM: extract `sample` method for Zones
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/test/java/abstractinterp/scalar/state/providers/ZoneDifferenceBoundedMatrixProvider.java
+++ src/test/java/abstractinterp/scalar/state/providers/ZoneDifferenceBoundedMatrixProvider.java
@@ -32,22 +32,26 @@ public class ZoneDifferenceBoundedMatrixProvider implements ArbitraryProvider {
 
     public Arbitrary<ZoneDifferenceBoundedMatrix> provideDBM() {
         Random r = new Random();
-        Arbitrary<ZoneDifferenceBoundedMatrix> matrix = Arbitraries.create(() -> {
-                Set<Local> ls = LocalProvider.generateLocals(r.nextInt(15) + 1);
-                ZoneDifferenceBoundedMatrix m = new ZoneDifferenceBoundedMatrix(ls, true);
-                for (Local s : ls) {
-                    for (Local t : ls) {
-                        // if they are not the same variable, 50% chance we add
-                        // a random constraint.
-                        if (!s.equals(t) && r.nextDouble() > 0.5) {
-                            Constraint c = ConstraintProvider.sample();
-                            m.setConstraint(s, t, c);
-                        }
-                    }
-                }
-                return m;
-            });
-
+        Arbitrary<ZoneDifferenceBoundedMatrix> matrix = Arbitraries.create(() -> sample(16, 0.5));
         return matrix;
+    }
+
+    public static ZoneDifferenceBoundedMatrix sample(int N, double density) {
+        return sample(N, density, System.currentTimeMillis());
+    }
+
+    public static ZoneDifferenceBoundedMatrix sample(int N, double density, long seed) {
+        Random r = new Random(seed);
+        Set<Local> ls = LocalProvider.generateLocals(N);
+        ZoneDifferenceBoundedMatrix m = new ZoneDifferenceBoundedMatrix(ls, true);
+        for (Local s : ls) {
+            for (Local t : ls) {
+                if (!s.equals(t) && r.nextDouble() < density) {
+                    Constraint c = ConstraintProvider.sample();
+                    m.setConstraint(s, t, c);
+                }
+            }
+        }
+        return m;
     }
 }
