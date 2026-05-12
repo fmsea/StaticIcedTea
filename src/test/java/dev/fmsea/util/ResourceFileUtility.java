11774fe9ea298c1cd2ed6f593a94649074db48a6
Kenny Ballou <kb@devnulllabs.io>
2026-05-11 18:52:07 -0700

    util: add resource list
    Small debug utility function for listing resources.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- src/test/java/dev/fmsea/util/ResourceFileUtility.java
+++ src/test/java/dev/fmsea/util/ResourceFileUtility.java
@@ -1,9 +1,13 @@
 package dev.fmsea.util;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResourceFileUtility {
     public static String readResourcesFile(String resourceFileName) {
@@ -13,6 +17,30 @@ public class ResourceFileUtility {
             return Files.readString(resourceFile);
         } catch (IOException ex) {
             System.err.println(ex.toString());
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static List<String> ls(String folderName) {
+        try {
+            URL resource = ResourceFileUtility.class.getClassLoader().getResource(folderName);
+            if (resource != null) {
+                Path path = Paths.get(resource.toURI());
+                return Files.list(path)
+                    .map(Path::toFile)
+                    .map(p -> p.getName())
+                    .collect(Collectors.toList());
+            } else {
+                System.err.println("URL was null");
+                return List.of();
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.toString());
+            ex.printStackTrace(System.err);
+            throw new RuntimeException(ex);
+        } catch (URISyntaxException ex) {
+            System.err.println(ex.toString());
+            ex.printStackTrace(System.err);
             throw new RuntimeException(ex);
         }
     }
